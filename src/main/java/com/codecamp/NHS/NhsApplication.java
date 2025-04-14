32c10ea470ad62d11aa37e8201739a6c94a304e3
JAA-252 <77969022+JAA-252@users.noreply.github.com>
2025-04-14 17:24:04 +0100

    first load

---
+++ src/main/java/com/codecamp/NHS/NhsApplication.java
@@ -0,0 +1,15 @@
+package com.codecamp.NHS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NhsApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/codecamp/NHS/NhsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecamp.NHS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
